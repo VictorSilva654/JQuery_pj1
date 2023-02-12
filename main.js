23b6ba594422443887e4cac2229dbbbbd7242f30
Victor Silva <46635431+VictorSilva654@users.noreply.github.com>
2023-02-11 21:05:04 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('header button').click(function(){
+        $('form').slideDown();
+    })
+
+    $('#button-cancel').click(function(){
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function(e){
+        e.preventDefault();
+        const novoEndereco = $("endereco-imagem-nova").val();
+        const novoItem = $('<li style="display: none"></li>');
+        $(` <img src="${novoEndereco}" />`).appendTo(novoItem);
+        $(`<div class="overlay-link">
+        <a href="${novoEndereco}" title="Ver imagem em tamanho real" target="_blank">Ver imagem em tamanho real</a>
+        </div>`).appendTo(novoItem);
+    $(novoItem).appendTo('ul');
+    $(novoItem).fadeIn(1000);
+    $('endereco-imagem-nova').val("");
+    })
+})
